5ff1fd2ba90dd942fe7a17d4617ce8662477be19
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2023-12-19 01:33:40 -0300

    Update mips1a.asm

--- mips1a.asm
+++ mips1a.asm
@@ -1,3 +1,17 @@
+# Arquitetura, Primeira VA, 2023.1
+# Grupo: Gabriela Rodrigues, Hudo Leonardo, Lucas Chagas, Louise d'Athayde
+# Questão: 1, letra A
+
+# Resumo: Esse código implementa a função 'strcpy' em assembly MIPS, que copia uma string de origem para uma string de destino,
+# incluindo o caractere de terminação NULL ('\0').
+# O código começa na função 'main', que carrega os endereços da string de origem ('source') e da string de destino ('target') nos 
+# registradores $a1 e $a0, e chama a função 'strcpy' usando 'jal strcpy'.
+# A função 'strcpy' inicia inicializando o registrador $v0 com o endereço de destino.
+# Em seguida, um loop copia byte a byte da string de origem para a string de destino, parando quando encontra o caractere nulo ('\0').
+# Após a cópia, o código retorna para a função chamadora ('main').
+
+
+
     .data
 source: .asciiz "Hello, world!"  # A string de origem
 target: .space 50                # Espaço reservado para a string de destino

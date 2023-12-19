91f0734e3e9e96477d368cdf1c5de6a143526450
Gabriela Maia <maiarodriguesgabriela@gmail.com>
2023-12-19 01:36:36 -0300

    Update and rename mips1b(2).asm to mips1b.asm

--- mips1b.asm
+++ mips1b.asm
@@ -1,3 +1,16 @@
+# Arquitetura, Primeira VA, 2023.1
+# Grupo: Gabriela Rodrigues, Hudo Leonardo, Lucas Chagas, Louise d'Athayde
+# Questão: 1, letra B
+
+# Resumo: Esse código implementa a função 'memcpy' em assembly MIPS.
+# A função 'memcpy' copia um número específico de bytes da origem para o destino.
+# No 'main', são carregados os endereços da string de origem ('source') e da string de destino ('dest')
+# nos registradores $a1 e $a0, respectivamente, e o número de bytes a serem copiados em $a2.
+# A função 'memcpy' usa os registradores $t0, $t1 e $t2 para manipular os ponteiros de origem e destino
+# e controlar a quantidade de bytes a serem copiados.
+# O loop 'memcpy_loop' copia byte a byte da origem para o destino até atingir o número de bytes especificado.
+# Após a cópia, o código retorna o endereço de destino em $v0 e retorna para a função chamadora ('main').
+
 .data
 source: .asciiz "Hello, World!"  # Origem (sequência de caracteres)
 dest:   .space 20                # Destino (buffer vazio com espaço para 20 bytes)
